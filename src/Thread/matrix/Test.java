6d8f47ecbdd8fba2bf80336b92a2c95cc92d9a5c
vinspier <17767160232@163.com>
2019-10-23 00:17:50 +0800

    添加多线程的串行和并行算法实例

--- src/Thread/matrix/Test.java
+++ src/Thread/matrix/Test.java
@@ -3,12 +3,15 @@ package Thread.matrix;
 public class Test {
     private static double[][] matrixA = MatrixGenerator.generate(500,2000);
     private static double[][] matrixB = MatrixGenerator.generate(2000,200);
-    private static double[][] matrixC = new double[500][200];
+    private static double[][] matrixC = new double[matrixA.length][matrixB[0].length];
 
     public static void main(String[] args) {
+       // System.out.println(Runtime.getRuntime().availableProcessors());
+
         testSerial();
         testConcurrent();
         testConcurrentPerRow();
+        testConcurrentRuntimeAvailable();
     }
 
     public static void testSerial(){
@@ -28,6 +31,13 @@ public class Test {
     public static void testConcurrentPerRow(){
         long start = System.currentTimeMillis();
         ConcurrentMultiply.multiplyPerRow(matrixA,matrixB,matrixC);
+        long end = System.currentTimeMillis();
+        System.out.println("testConcurrentPerRow matrix calculate takes time : " + (end - start));
+    }
+
+    public static void testConcurrentRuntimeAvailable(){
+        long start = System.currentTimeMillis();
+        ConcurrentMultiply.multiplyRuntimeAvailable(matrixA,matrixB,matrixC);
         long end = System.currentTimeMillis();
         System.out.println("testConcurrentPerRow matrix calculate takes time : " + (end - start));
     }
